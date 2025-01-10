db3712d5b303ed349a7c7ac92b3206a5140d72a5
Oleksii Pavlenko <alexeypavlenko77@gmail.com>
2025-01-10 04:17:02 +0200

    Lesson 18 JS start

---
+++ lesson-18-js-start/script.js
@@ -0,0 +1,71 @@
+"use strict";
+
+let a = 15;
+let b = 6;
+
+let a1 = 10;
+let a2 = a - a1; // 5
+
+let result = a1 * b + a2 * b;
+console.log(result);
+
+// Old staff
+var q = 5;
+
+// ES6 (2015)
+const w = 5;
+let e = 5;
+
+
+let my_super_variable;
+let mySuperVariable;
+const MySuperVariable = 20;
+
+my_super_variable = 20;
+mySuperVariable = 20;
+
+
+let yyy = 300;
+console.log(yyy);
+
+let speedometerValue = "500"; // Looks like number but it's a string !!!
+
+console.log(name);
+console.log(typeof name);
+
+name = 200;
+console.log(name);
+console.log(typeof name);
+
+const isYyyEqualsThreeHundred = yyy === 600;
+
+let hasDrivingLicence = true;
+
+console.log(isYyyEqualsThreeHundred);
+
+let something = null;
+console.log(something);
+
+let student1 = 'Eugene';
+let student2 = 'Max';
+let student3 = 'Oleksandr';
+let student4 = 'Yuri';
+
+let studentsFromNovGroup = [ 'Eugene', 'Max', 'Oleksandr', 'Yuri' ];
+console.log(studentsFromNovGroup[1]);
+
+let personName = 'John';
+let personEmail = 'john@gmail.com'; // This is John's email
+let personPhoneNumber = '+38097123456';
+let personAge = 42;
+
+
+let person = {
+  name: 'John',
+  email: 'john@gmail.com',
+  phoneNumber: '+38097123456',
+  age: 42,
+};
+
+console.log(person.email);
+console.log(person['age']);
